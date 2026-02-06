3a42c7ada078ce83b2567456787eedac6237537e
GeetikaSaini13 <geetikasaini1307@gmail.com>
2026-02-06 23:14:36 +0530

    Experiment 4

---
+++ Experiments/EcoTrack/src/pages/Overview.jsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography';
+
+const Overview = () => {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Overview Page
+      </Typography>
+      <Typography paragraph>
+        This is the Overview section of EcoTrack.
+      </Typography>
+    </div>
+  );
+};
+
+export default Overview;
---
+++ Experiments/EcoTrack/src/pages/Reports.jsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography';
+
+const Reports = () => {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Reports & Analytics
+      </Typography>
+      <Typography paragraph>
+        Detailed reports and carbon footprint analysis coming soon...
+      </Typography>
+    </div>
+  );
+};
+
+export default Reports;
